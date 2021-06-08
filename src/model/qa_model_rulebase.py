6dede673c9a737c1e15da81411311c76c478227d
B06901141 <b06901141@ntu.edu.tw>
2021-06-08 13:14:33 +0000

    public score 0.6872

--- src/model/qa_model_rulebase.py
+++ src/model/qa_model_rulebase.py
@@ -5,46 +5,58 @@ import re
 
 def is_inv(sent:str):
     return bool(re.search('錯|誤|不|沒|(非(?!常|洲))',sent))
-def get_sim(sent:str, doc:list):
-    sim = np.array([edit_distance.SequenceMatcher(i, sent, action_function=edit_distance.highest_match_action).matches() \
-                                 for i in doc], dtype=np.float32)
 
-    sim -= sim[sim <= np.percentile(sim, 80)].mean()
-    sim[sim < 0] = 0
+def get_sim(sent:str, doc:list):
+    match_sm = [edit_distance.SequenceMatcher(i, sent, action_function=edit_distance.highest_match_action) for i in doc]
+    match_score = np.array([i.matches() for i in match_sm], dtype=np.float32)
+    match_score -= match_score[match_score <= np.percentile(match_score, 80)].mean()
+    match_score[match_score < 0] = 0
 
     # _filter = [1,0.5,0.25]
-    # sim = np.convolve(sim, _filter, 'full')[:-len(_filter) + 1]
+    # match_score = np.convolve(match_score, _filter, 'full')[:-len(_filter) + 1]
     
     # sent_inv = is_inv(sent)
     # inv = [is_inv(i) ^ sent_inv for i in doc]
-    # sim[inv] *= -1
+    # match_score[inv] *= -1
 
-    # sim /= len(sent)
+    # match_score /= len(sent)
     _filter = [1, 0.5, 0.25]
-    sim = np.convolve(sim, _filter, 'full')[:-len(_filter) + 1]
+    match_score = np.convolve(match_score, _filter, 'full')[:-len(_filter) + 1]
     
-    return sim
-def get_correct(sent:str, doc:list):
-    correct = np.array([edit_distance.SequenceMatcher(i, sent, action_function=edit_distance.highest_match_action).matches() \
-                                 for i in doc], dtype=np.float32) / len(sent)
+    return match_score
 
-    correct -= correct[correct <= np.percentile(correct, 80)].mean()
-    correct[correct < 0] = 0
+def get_correct(sent:str, doc:list):
+    match_sm = [edit_distance.SequenceMatcher(i, sent, action_function=edit_distance.highest_match_action) for i in doc]
+    match_score = np.array([i.matches() for i in match_sm], dtype=np.float32)
     
+    # match_seq = []
+    match_len = []
+    for sm, s in zip(match_sm, doc):
+        blocks = [*sm.get_matching_blocks()]
+        if len(blocks) == 0:
+            # match_seq.append("")
+            match_len.append(0)
+        else:
+            # match_seq.append(s[blocks[0][0]:blocks[-1][0] + 1])
+            match_len.append(blocks[-1][0] - blocks[0][0] + 1)
+
+    match_score = match_score ** 2 / (np.array(match_len) + 1e-10)
+    match_score -= match_score.mean()
+
     _filter = [1, 0.6, 0.36]
-    correct = np.convolve(correct, _filter, 'full')[:-len(_filter) + 1]
+    match_score = np.convolve(match_score, _filter, 'full')[:-len(_filter) + 1]
 
     sent_inv = is_inv(sent)
     inv = [is_inv(i) ^ sent_inv for i in doc]
-    correct[inv] *= -1
+    match_score[inv] *= -1
 
-    # correct /= len(sent)
-    # correct -= correct.mean()
+    # match_score /= len(sent)
+    # match_score -= match_score.mean()
 
     _filter = [1, 0.6, 0.36]
-    correct = np.convolve(correct, _filter, 'full')[:-len(_filter) + 1]
+    match_score = np.convolve(match_score, _filter, 'full')[:-len(_filter) + 1]
 
-    return correct.max()
+    return match_score.max()
 
 class RuleBaseQA():
     def predict(self, dataset):
